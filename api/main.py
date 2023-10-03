2166095c414b31e5663f57bc1dde0bc15d0f789e
Jon McLean <jon@mclean.one>
2023-10-03 14:08:13 +1100

    Updated api to use connection pool dispatcher

--- api/main.py
+++ api/main.py
@@ -8,46 +8,44 @@ from flask_socketio import SocketIO, emit
 
 import redis
 
-from redis_handler import RedisHandler
-from socket_handler import SocketHandler
+from redis.connection_handler import RedisConnectionDispatcher
+# from redis.redis_connection import RedisConnection
+# from socket_handler import SocketHandler
 
 app = Flask(__name__)
-app.config["SECRET"] = os.environ("API_SECRET")
+app.config["SECRET"] = os.environ.get("API_SECRET")
 
 socketio = SocketIO(app)
 
-redisHandler = RedisHandler() # Create handler with default
-socketHandler = SocketHandler(socketio, redisHandler)
+dispatcher = RedisConnectionDispatcher() # Create handler with default
+# socketHandler = SocketHandler(socketio, redisHandler)
 
 
+@socketio.on("connect")
 def connect():
     print("Connected to websocket")
     emit("response", {"data": "connected", "status": 0})
 
-uuid = None
+@socketio.on("start_trans")
+def start_transmission(identifier):
+    connection = dispatcher.get_connection()
+    res = connection.check_transmission_exists(identifier)
 
-@socketio.on("begin")
-def received_begin():
-    if socketHandler.transmission_in_progress:
-        emit("response", {"data": "Transmission already in progress", "status": 1})
-    else:
-        uuid = socketHandler.create_transmission()
-        emit("response", {"data": uuid, "status": 0})
+    if res:
+        emit("response", {"error": "Already exists", "status": 406})
+    else: # Transmission does not exist
+        connection.create_transmission(identifier)
+        emit("response", {"data": "Transmission created", "status": 200})
 
-@socketio.on("image")
-def received_image(encoded_image):
-    if uuid is None:
-        emit("response", {"data": "No transmission in progress", "status": 1})
-    else:
-        uuid = socketHandler.handle_image(uuid, encoded_image)
-        emit("response", {"transmission_id": uuid, "status": 0})
+@socketio.on("submit_frame")
+def submit_frame_for_transmission(identifier, image):
+    connection = dispatcher.get_connection()
+    res = connection.check_transmission_exists(identifier)
 
-@socketio.on("end")
-def received_end(uuid):
-    if uuid is None:
-        emit("response", {"data": "No transmission in progress", "status": 1})
+    if res:
+        connection.set_transmission_block(identifier, image)
+        emit("response", {"data": "Frame submitted", "status": 200})
     else:
-        socketHandler.verify_transmission(uuid)
-        emit("response", {"data": "Transmission ended", "status": 0})
+        emit("response", {"error": "Transmission does not exist", "status": 404})
 
 
