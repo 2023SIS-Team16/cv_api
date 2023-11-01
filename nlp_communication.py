f72d0379b16a58fdd374dd117ada902471f730bb
Jon McLean <jon@mclean.one>
2023-11-01 16:46:17 +1100

    Added back NLP
    Forgot to commit after my testing

--- nlp_communication.py
+++ nlp_communication.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import main
+# import main
 
 #For communicating with NLP over HTTP requests
 request_url = "http://127.0.0.1:5000/parse_text"
@@ -8,6 +8,9 @@ request_url = "http://127.0.0.1:5000/parse_text"
 #To be called once the model has found a new letter
 class Communication:
     string = ""
+
+    def new_letter_no_conversion(self, letters):
+        self.string = self.string + ''.join(letters)
 
     def new_letter(self, letter):
         print(f"New Letter: {letter}")
@@ -17,18 +20,21 @@ class Communication:
         input = {
             "text": self.string,
             "index": 0,
-            "truncate": True,
+            "truncate": False,
         }
         json_input = json.dumps(input)
-
         response = requests.get(request_url, json=json_input)
-        print(response.content)
+        # print(response)
+        # print(response.content)
         content = json.loads(response.content)
-        print(content)
+        # print(content)
 
         #Truncate string if there's a response
-        self.string = self.string[content['index']:]
-        
+        # self.string = self.string[content['index']:]
+        print(f"String: {self.string}")
+
+        print(content.keys())
+
         if "text" in content:
             print("Message emitted")
             print(content['text'])
